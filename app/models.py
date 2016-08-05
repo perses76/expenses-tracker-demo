10641d42ec93d2dc798f72e3069bda89ad2c6dae
Vadim Ovcearenco <wadim@veles-soft.com>
2016-08-05 13:59:10 +0300

    Added Expense model and migrations for it

--- app/models.py
+++ app/models.py
@@ -5,3 +5,8 @@ Definition of models.
 from django.db import models
 
 # Create your models here.
+class Expense(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Amount', null=False)
+    description = models.CharField(max_length=1000, null=False, blank=True)
+    comment = models.TextField(null=False, blank=True)
+    create_dt = models.DateTimeField(auto_now_add=True, null=False)
