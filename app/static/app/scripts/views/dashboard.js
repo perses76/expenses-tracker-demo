7591e27a2c4625d1526f5a949bd7caf6d815804a
Vadim Ovcearenco <wadim@veles-soft.com>
2016-08-09 08:59:44 +0300

    Show users view on dashboard

--- app/static/app/scripts/views/dashboard.js
+++ app/static/app/scripts/views/dashboard.js
@@ -1,18 +1,34 @@
-define(['underscore', 'backbone', 'text!templates/dashboard.html', 'views/expenses'],
-    function (_, BB, template_str, ExpensesView) {
+define(['underscore', 'backbone', 'text!templates/dashboard.html', 'views/expenses', 'views/users'],
+    function (_, BB, template_str, ExpensesView, UsersView) {
     return BB.View.extend({
         template: _.template(template_str),
+        state: 'expenses',
         events: {
             'click #logout_btn': 'on_login_click',
+            'click #expenses_btn': 'on_expenses_click',
+            'click #users_btn': 'on_users_click',
         },
         on_login_click: function () {
             this.trigger('logout');
         },
+        on_expenses_click: function () {
+            this.state = 'expenses';
+            this.render();
+        },
+        on_users_click: function () {
+            this.state = 'users';
+            this.render();
+        },
         render: function () {
+            var content_view;
             this.$el.html(this.template());
-            var expenses_view = new ExpensesView();
-            this.$('#dashboard_content').append(expenses_view.$el);
-            expenses_view.render();
+            if (this.state == 'expenses') {
+                content_view = new ExpensesView();
+            } else {
+                content_view = new UsersView();
+            }
+            this.$('#dashboard_content').append(content_view.$el);
+            content_view.render();
             return this.$el;
         }
     })
