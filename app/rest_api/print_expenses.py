563af6e180b4025a0a1d8f986f6d890557fb62f2
Vadim Ovcearenco <wadim@veles-soft.com>
2016-08-10 14:25:48 +0300

    Implement print  functionality

--- app/rest_api/print_expenses.py
+++ app/rest_api/print_expenses.py
@@ -8,15 +8,42 @@ from expense import get_expenses_query_set
 from django.shortcuts import render
 
 
+def are_dates_equal(dt1, dt2):
+    if dt1 is None or dt2 is None:
+        return False
+    return dt1.isocalendar()[1] == dt2.isocalendar()[1]
+
+
 # @resource.user_authentication_required
 def print_expenses(request):
     qs = get_expenses_query_set(request)
-    data = {
-        'items': list(qs),
-    }
+    expenses = qs.order_by('transaction_dt')
+
+    # group expenses
+    groups = []
+    current_dt = None
+    group = None
+    for exp in expenses:
+        if not are_dates_equal(current_dt, exp.transaction_dt):
+            if group is not None:
+                groups.append(group)
+            group = {
+                'transaction_dt': exp.transaction_dt,
+                'expenses': []
+            }
+            current_dt = exp.transaction_dt
+        group['expenses'].append(exp)
+    if group is not None:
+        groups.append(group)
+
+    # calculate aggregates
+    for g in groups:
+        total = sum([exp.amount for exp in g['expenses']])
+        g.update({'total': total, 'average': total / len(g['expenses'])})
+
     return render(
         request,
         'app/print_expenses.html',
-        data
+        {'groups': groups}
     )
     return http.HttpResponse(content, status=200)
--- app/static/app/scripts/views/print_expenses.js
+++ app/static/app/scripts/views/print_expenses.js
@@ -9,9 +9,20 @@ define([
         render: function () {
             var view = this;
             var $iframe = $('<iframe src="/api/print_expenses" name="print_expenses"></iframe>');
+            $iframe.hide();
             this.$el.append($iframe);
-            // window.frames['print_expenses'].print();
+            window.frames['print_expenses'].print();
+            // this.remove();
             return;
+        },
+        render1: function () {
+            w = window.open('/api/print_expenses', '_blank');
+            w.onunload = function(){
+                console.log('closed!');
+            }
+            w.focus();
+            w.print();
+            // w.close();
         }
     });
 });
