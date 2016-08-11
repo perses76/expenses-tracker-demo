bff086b1006f4f039aa56e5ee4af1f6c76c7dc83
Vadim Ovcearenco <wadim@veles-soft.com>
2016-08-11 16:47:20 +0300

    Fix print_expsneses UI

--- app/rest_api/print_expenses.py
+++ app/rest_api/print_expenses.py
@@ -6,6 +6,10 @@ import resource
 from dateutil import parser
 from expense import get_expenses_query_set
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+import resource
+
+User = get_user_model()
 
 
 def are_dates_equal(dt1, dt2):
@@ -13,6 +17,10 @@ def are_dates_equal(dt1, dt2):
         return False
     return dt1.isocalendar()[1] == dt2.isocalendar()[1]
 
+
+def get_group_caption(dt):
+    year, week, weekday = dt.isocalendar()
+    return '{week} week, {year}'.format(year=year, week=week)
 
 @resource.user_authentication_required
 def print_expenses(request):
@@ -29,6 +37,7 @@ def print_expenses(request):
                 groups.append(group)
             group = {
                 'transaction_dt': exp.transaction_dt,
+                'caption': get_group_caption(exp.transaction_dt),
                 'expenses': []
             }
             current_dt = exp.transaction_dt
@@ -40,10 +49,15 @@ def print_expenses(request):
     for g in groups:
         total = sum([exp.amount for exp in g['expenses']])
         g.update({'total': total, 'average': total / len(g['expenses'])})
+    
+    user = request.user
+    if 'user' in request.GET:
+        user_id = long(request.GET['user'])
+        user = User.objects.get(id=user_id)
 
     return render(
         request,
         'app/print_expenses.html',
-        {'groups': groups}
+        {'groups': groups, 'user': user}
     )
     return http.HttpResponse(content, status=200)
