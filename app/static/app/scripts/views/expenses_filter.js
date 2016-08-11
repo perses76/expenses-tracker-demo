7b825d2b7ef8a906971c435e2dfc4409334aa21c
Vadim Ovcearenco <wadim@veles-soft.com>
2016-08-11 11:36:05 +0300

    BugFix. Dont fill user select box for regular user

--- app/static/app/scripts/views/expenses_filter.js
+++ app/static/app/scripts/views/expenses_filter.js
@@ -32,20 +32,21 @@ define([
             var role_input = this.$('.role_input');
             role_input.append('<option value="' + user.id + '">' + user.get('first_name') + ' ' + user.get('last_name') + '</option>');
             role_input.val(user.id);
-            var user_collection = new UserCollection();
-            user_collection.fetch({
-                success: function (collection) {
-                    role_input.empty();
-                    collection.each(function (item) {
-                        role_input.append('<option value="' + item.id + '">' + item.get('first_name') + ' ' + item.get('last_name') + '</option>');
-                    });
-                    role_input.val(user.id);
-                },
-                error: function () {
-                    alert('error!');
-                }
-
-            });
+            if (user.get('role') == 'admin') {
+                var user_collection = new UserCollection();
+                user_collection.fetch({
+                    success: function (collection) {
+                        role_input.empty();
+                        collection.each(function (item) {
+                            role_input.append('<option value="' + item.id + '">' + item.get('first_name') + ' ' + item.get('last_name') + '</option>');
+                        });
+                        role_input.val(user.id);
+                    },
+                    error: function () {
+                        alert('error!');
+                    }
+                });
+            }
         }
     });
 
